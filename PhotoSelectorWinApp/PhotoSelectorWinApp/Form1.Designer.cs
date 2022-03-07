bf0efc3a9b455ca0b39c93d89bc0695fba651370
jeet404 <jeet63240@gmail.com>
2022-03-07 20:35:35 +0530

    Add new PhotoViewer with openFileDialog .

---
+++ PhotoSelectorWinApp/PhotoSelectorWinApp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PhotoSelectorWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.btn_choose = new System.Windows.Forms.Button();
+            this.lbl_test = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(12, 12);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(909, 506);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // btn_choose
+            // 
+            this.btn_choose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_choose.Location = new System.Drawing.Point(416, 524);
+            this.btn_choose.Name = "btn_choose";
+            this.btn_choose.Size = new System.Drawing.Size(130, 31);
+            this.btn_choose.TabIndex = 1;
+            this.btn_choose.Text = "Choose Pic";
+            this.btn_choose.UseVisualStyleBackColor = true;
+            this.btn_choose.Click += new System.EventHandler(this.btn_choose_Click);
+            // 
+            // lbl_test
+            // 
+            this.lbl_test.AutoSize = true;
+            this.lbl_test.Location = new System.Drawing.Point(12, 534);
+            this.lbl_test.Name = "lbl_test";
+            this.lbl_test.Size = new System.Drawing.Size(0, 13);
+            this.lbl_test.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 562);
+            this.Controls.Add(this.lbl_test);
+            this.Controls.Add(this.btn_choose);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.Button btn_choose;
+        private System.Windows.Forms.Label lbl_test;
+    }
+}
+
---
+++ PhotoSelectorWinApp/PhotoSelectorWinApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoSelectorWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_choose_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            string path = fd.FileName;
+            lbl_test.Text = path;
+            pic1.Load(path);
+        }
+    }
+}
