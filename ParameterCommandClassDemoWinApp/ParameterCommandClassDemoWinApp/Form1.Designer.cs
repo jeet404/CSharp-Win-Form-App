d2db1271a87f185846a966b6e8631875668b0bac
jeet404 <jeet63240@gmail.com>
2022-03-15 20:08:13 +0530

    Add New Form Which Inserts data  by  Parameters.

---
+++ ParameterCommandClassDemoWinApp/ParameterCommandClassDemoWinApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ParameterCommandClassDemoWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.txt_unm = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.lbl_stat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.Location = new System.Drawing.Point(124, 94);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(194, 26);
+            this.txt_pass.TabIndex = 12;
+            // 
+            // txt_unm
+            // 
+            this.txt_unm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_unm.Location = new System.Drawing.Point(124, 62);
+            this.txt_unm.Name = "txt_unm";
+            this.txt_unm.Size = new System.Drawing.Size(194, 26);
+            this.txt_unm.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 33);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Login Form";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save.Location = new System.Drawing.Point(105, 142);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(124, 33);
+            this.btn_save.TabIndex = 7;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // lbl_stat
+            // 
+            this.lbl_stat.AutoSize = true;
+            this.lbl_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_stat.Location = new System.Drawing.Point(259, 218);
+            this.lbl_stat.Name = "lbl_stat";
+            this.lbl_stat.Size = new System.Drawing.Size(59, 20);
+            this.lbl_stat.TabIndex = 13;
+            this.lbl_stat.Text = "          ";
+            this.lbl_stat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 247);
+            this.Controls.Add(this.lbl_stat);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_unm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_save);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_unm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Label lbl_stat;
+    }
+}
+
---
+++ ParameterCommandClassDemoWinApp/ParameterCommandClassDemoWinApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ParameterCommandClassDemoWinApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("INSERT INTO user_detail VALUES(@u_name,@u_pass)",conn);
+            cmd.Parameters.AddWithValue("@u_name", txt_unm.Text);
+            cmd.Parameters.AddWithValue("@u_pass", txt_pass.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Record Inserted Successfully");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB; AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\ParameterCommandClassDemoWinApp\ParameterCommandClassDemoWinApp\User_Data.mdf'; Integrated Security = True");
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    lbl_stat.BackColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                    lbl_stat.BackColor = Color.Red;
+            }
+        }
+    }
+}
