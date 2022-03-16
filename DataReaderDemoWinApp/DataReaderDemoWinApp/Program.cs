bd043b74b2a7a3b1d655a09598354efe3045c048
jeet404 <jeet63240@gmail.com>
2022-03-16 11:23:56 +0530

    Update File.

---
+++ DataReaderDemoWinApp/DataReaderDemoWinApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\ParameterCommandClassDemoWinApp\ParameterCommandClassDemoWinApp\User_Data.mdf';Integrated Security=True");
+        conn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM user_detail", conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine(" |- "+reader["u_name"].ToString()+" "+reader["u_pass"].ToString());
+        }
+        Console.ReadLine();
+    }
+}
+
