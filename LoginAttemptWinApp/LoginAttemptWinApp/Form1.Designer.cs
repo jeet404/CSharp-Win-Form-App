db897deebe8b6128a35db89c107fa057cde0178b
jeet404 <jeet63240@gmail.com>
2022-03-17 15:02:46 +0530

    Add New Program for Attempting Login Sessions in C# windows Form App.

---
+++ LoginAttemptWinApp/LoginAttemptWinApp/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace LoginAttemptWinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.txt_unm = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lbl_stat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pass.Location = new System.Drawing.Point(114, 105);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(194, 26);
+            this.txt_pass.TabIndex = 11;
+            // 
+            // txt_unm
+            // 
+            this.txt_unm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_unm.Location = new System.Drawing.Point(114, 73);
+            this.txt_unm.Name = "txt_unm";
+            this.txt_unm.Size = new System.Drawing.Size(194, 26);
+            this.txt_unm.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(91, 188);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(124, 33);
+            this.btn_login.TabIndex = 7;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // lbl_stat
+            // 
+            this.lbl_stat.AutoSize = true;
+            this.lbl_stat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_stat.Location = new System.Drawing.Point(6, 9);
+            this.lbl_stat.Name = "lbl_stat";
+            this.lbl_stat.Size = new System.Drawing.Size(59, 20);
+            this.lbl_stat.TabIndex = 12;
+            this.lbl_stat.Text = "          ";
+            this.lbl_stat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 258);
+            this.Controls.Add(this.lbl_stat);
+            this.Controls.Add(this.txt_pass);
+            this.Controls.Add(this.txt_unm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_unm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lbl_stat;
+    }
+}
+
---
+++ LoginAttemptWinApp/LoginAttemptWinApp/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginAttemptWinApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter da;
+        DataTable dt;
+        int attempt = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("SELECT * FROM user_data WHERE username = '"+txt_unm.Text+"' and password = '"+txt_pass.Text+"'", conn);
+            dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Login Successfully");
+            }
+            else
+            {
+                attempt--;
+                MessageBox.Show("Not Matched, "+attempt.ToString()+" Attempt Left!");
+
+            }
+            if (attempt == 0)
+            {
+                txt_unm.Enabled = false;
+                txt_pass.Enabled = false;
+                btn_login.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\LoginAttemptWinApp\LoginAttemptWinApp\users.mdf';Integrated Security=True");
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    lbl_stat.BackColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                lbl_stat.BackColor = Color.Red;
+            }
+        }
+    }
+}
