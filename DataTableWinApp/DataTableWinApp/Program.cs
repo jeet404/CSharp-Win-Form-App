0f6b7fd40c96a4e36ec4ddf158ab4f9c7ae412be
jeet404 <jeet63240@gmail.com>
2022-03-19 14:07:58 +0530

    Add New Program of DataTable.

---
+++ DataTableWinApp/DataTableWinApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+class main
+{
+    public static void Main()
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\ParameterCommandClassDemoWinApp\ParameterCommandClassDemoWinApp\User_Data.mdf';Integrated Security=True");
+        conn.Open();
+        SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM user_detail", conn);
+        DataTable dt = new DataTable();
+        da.Fill(dt);
+        for(int i = 0; i < dt.Rows.Count; i++)
+        {
+            Console.WriteLine(dt.Rows[i][0].ToString()+" "+dt.Rows[i][1].ToString());
+        }
+        Console.ReadLine();
+    }
+}
