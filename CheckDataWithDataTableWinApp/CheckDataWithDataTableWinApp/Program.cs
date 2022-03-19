66d1ad186424a5b72099de43e13323f4a1c37497
jeet404 <jeet63240@gmail.com>
2022-03-19 14:40:39 +0530

    Add DataSet Program And Another Program of DataTable.

---
+++ CheckDataWithDataTableWinApp/CheckDataWithDataTableWinApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+class main
+{
+    public static void Main()
+    {
+        int chkid;
+        int choice;
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\ParameterCommandClassDemoWinApp\ParameterCommandClassDemoWinApp\User_Data.mdf';Integrated Security=True");
+        conn.Open();
+        do
+        {
+            Console.Write("Enter Id : ");
+            chkid = Int32.Parse(Console.ReadLine());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM user_detail WHERE Id = "+chkid+"", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Console.WriteLine("Data Found");
+                Console.WriteLine("_____________");
+                Console.WriteLine("Id = "+dt.Rows[0][0].ToString());
+                Console.WriteLine("Username = "+dt.Rows[0][1].ToString());
+                Console.WriteLine("Password = "+dt.Rows[0][2].ToString());
+                Console.WriteLine("_____________");
+            }
+            else
+            {
+                Console.WriteLine("Data Not Exist!!");
+            }
+            Console.WriteLine("For To Continue Press 1...");
+            choice = Int32.Parse(Console.ReadLine());
+        }while(choice == 1);
+    }
+}
---
+++ DataSetDemoWinApp/DataSetDemoWinApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+class main
+{
+    public static void Main()
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\WorkSpace\BCA SEM-4\C# Form App\DataSetDemoWinApp\DataSetDemoWinApp\main_data.mdf';Integrated Security=True");
+        conn.Open();
+        SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM stud", conn);
+        DataSet ds = new DataSet();
+        da.Fill(ds,"stud");
+        SqlDataAdapter da2 = new SqlDataAdapter("SELECT * FROM emp", conn);
+        da2.Fill(ds, "emp");
+        Console.WriteLine("=== STUD ===");
+        Console.WriteLine();
+        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+        {
+            Console.WriteLine(ds.Tables[0].Rows[i][0].ToString()+" "+ds.Tables[0].Rows[i][1].ToString());
+        }
+        Console.WriteLine();
+        Console.WriteLine("=== EMP ===");
+        Console.WriteLine();
+        for (int i = 0; i < ds.Tables[1].Rows.Count;i++)
+        {
+            Console.WriteLine(ds.Tables[1].Rows[i][0].ToString()+" "+ds.Tables[1].Rows[i][1].ToString());
+        }
+    }
+}
