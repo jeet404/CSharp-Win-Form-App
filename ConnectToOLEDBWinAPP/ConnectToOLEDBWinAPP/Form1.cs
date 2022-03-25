d0d85aa209e428207bdfec94f4e07f48950bf9c5
jeet404 <jeet63240@gmail.com>
2022-03-25 22:06:55 +0530

    Add New App For OLEDB.

---
+++ ConnectToOLEDBWinAPP/ConnectToOLEDBWinAPP/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ConnectToOLEDBWinAPP
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\WorkSpace\BCA SEM-4\C# Form App\ConnectToOLEDBWinAPP\ConnectToOLEDBWinAPP\Database1.accdb"); 
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    lbl_stat.BackColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.Message;
+                lbl_stat.BackColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ ConnectToOLEDBWinAPP/ConnectToOLEDBWinAPP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConnectToOLEDBWinAPP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConnectToOLEDBWinAPP")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9bdc35de-3b40-443e-a1bf-88921b2ba243")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
