78c9b1f2defa397284e60ada897b6ddf47151f7b
jeet404 <jeet63240@gmail.com>
2022-03-31 17:35:18 +0530

    Update Files And Make new File For Custom User Control

---
+++ TextboxMobileWinAapp/TextboxMobileWinAapp/UserControl1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TextboxMobileWinAapp
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_onlynum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_onlynum
+            // 
+            this.txt_onlynum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_onlynum.Location = new System.Drawing.Point(6, 3);
+            this.txt_onlynum.Name = "txt_onlynum";
+            this.txt_onlynum.Size = new System.Drawing.Size(233, 26);
+            this.txt_onlynum.TabIndex = 0;
+            this.txt_onlynum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_onlynum_KeyPress);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txt_onlynum);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(245, 31);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_onlynum;
+    }
+}
---
+++ TextboxMobileWinAapp/TextboxMobileWinAapp/UserControl1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextboxMobileWinAapp
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_onlynum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(e.KeyChar > 48 && e.KeyChar < 57 || e.KeyChar == 8))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
